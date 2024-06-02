a90bd48fb198f79624f577d4a3c77aa31cbab7fd
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-06-02 17:43:36 +0600

    first-commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	/*-- =====Menu Bar Active start==== --*/
+
+	$('.nav-list').click(function(){
+		$('.nav-list.active').removeClass('active');
+		$(this).addClass('active')
+	});
+
+
+});   
+
+
